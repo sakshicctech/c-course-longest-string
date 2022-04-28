1cb80b5e373c6b30081bc6dc0e67407849a4730f
sakshicctech <sakshi.thorat@cctech.co.in>
2022-04-28 22:32:40 +0530

    changes added

--- longest_string.c
+++ longest_string.c
@@ -4,21 +4,23 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc == 1)
-    puts(" ");
-
-    if (argc == 2)
-    puts(argv[1]);
-
-    if (argc > 2) {
-        for (int i = 1; i < argc ; i++) {
-            if (strlen(argv[1]) < strlen(argv[i]))
-            {
-                argv[1] = argv[i];
-            }
-
-        }
-        printf("%s", argv[1]);
+    
+    if (argc == 2) {
+        fputs(argv[1], stdout);
     }
     
+    if (argc > 2) {
+        char *lstr = argv[1];
+        for (int i = 1; i < argc ; i++) {
+            if (strlen(argv[i]) > strlen(lstr))
+            {
+                lstr = argv[i];
+            }
+        }
+        fputs(lstr, stdout);
+    }
+
+    else {
+        puts("");
+    }
 }
